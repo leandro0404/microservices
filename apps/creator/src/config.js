afcf32827dd67e537a63df031ea35eddc5f81d1d
leandro0404 <leandrobhmgg@gmail.com>
2023-10-01 21:53:22 -0300

    apps

--- apps/creator/src/config.js
+++ apps/creator/src/config.js
@@ -1,8 +1,10 @@
 export const CONFIG = {
-    "RESPONSE_TYPE": "code",
-    "SCOPE": "openid profile",
-    "REDIRECT_URI": "http://localhost:3000",
-    "CLIENT_ID": "app-site-b",
-    "CLIENT_SECRET": "secret",
-    "GRANT_TYPE": "authorization_code"
+  "RESPONSE_TYPE": process.env.REACT_APP_RESPONSE_TYPE,
+  "SCOPE": process.env.REACT_APP_SCOPE,
+  "REDIRECT_URI": process.env.REACT_APP_REDIRECT_URI,
+  "CLIENT_ID": process.env.REACT_APP_CLIENT_ID,
+  "CLIENT_SECRET": process.env.REACT_APP_CLIENT_SECRET,
+  "GRANT_TYPE": process.env.REACT_APP_GRANT_TYPE,
+  "BASIC_TOKEN": process.env.REACT_APP_BASIC_TOKEN,
+  "AUTH_SERVER": process.env.REACT_APP_AUTH_SERVER
 };
--- apps/creator/src/services/tokenService.js
+++ apps/creator/src/services/tokenService.js
@@ -3,7 +3,7 @@ import {CONFIG} from "../config";
 
 
 export const sendAuthorizationRequest = () => {
-    let authorizeRequest = `http://localhost:9002/oauth2/authorize?response_type=${ CONFIG.RESPONSE_TYPE }&scope=${ CONFIG.SCOPE }&redirect_uri=${ CONFIG.REDIRECT_URI }&client_id=${ CONFIG.CLIENT_ID }`;
+    let authorizeRequest = `${ CONFIG.AUTH_SERVER }/oauth2/authorize?response_type=${ CONFIG.RESPONSE_TYPE }&scope=${ CONFIG.SCOPE }&redirect_uri=${ CONFIG.REDIRECT_URI }&client_id=${ CONFIG.CLIENT_ID }`;
     window.location.href = authorizeRequest;
 };
 
@@ -17,13 +17,13 @@ export const sendTokenRequest = (code) => {
 
 
     const headers = {
-        "Authorization": "Basic YXBwLXNpdGUtYjpzZWNyZXQ=",
+        "Authorization": CONFIG.BASIC_TOKEN,
         "Content-Type": "application/x-www-form-urlencoded",
         "Accept": "application/json",
       };
     
       return axios
-        .post(`http://localhost:9002/oauth2/token`, body.join("&"), { headers })
+        .post(`${ CONFIG.AUTH_SERVER }/oauth2/token`, body.join("&"), { headers })
         .then((response) => {
           if (response.status !== 200) {
             return Promise.reject(
